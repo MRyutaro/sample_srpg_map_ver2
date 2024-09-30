ee982390ccb78068b54116fb53885dd97b723555
MRyutaro <m2002r1028@icloud.com>
2024-09-30 23:30:58 +0900

    add: ref

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef, useEffect } from "react";
 import image from "../image1.png";
 
 // マップ全体の設定
@@ -7,8 +7,8 @@ const MAP_TILES_Y_LEN = 20; // マップの縦のタイル数（windowのY軸方
 const TILE_SIZE = 100; // タイルのサイズ
 const ROTATE_X = 66; // マップの平たさ具合(deg)
 const A = TILE_SIZE * 0.71; // タイルを敷き詰めるための補正値。√2/2 ≒ 0.71。これを変えるとタイル同士の間隔が変わる。
-const MAP_LEFT_OFFSET = 800; // マップの左のオフセット
-const MAP_TOP_OFFSET = -300; // マップの上のオフセット
+const MAP_LEFT_OFFSET = 0; // マップの左のオフセット
+const MAP_TOP_OFFSET = 0; // マップの上のオフセット
 
 // タイルの設定
 const SELECTED_TILE_MOVE_Y = -5; // タイルが選択されたときのY軸の移動量(px)
@@ -18,12 +18,12 @@ const IMAGE_POSITION = -296; // 画像の位置
 
 export function Map(): JSX.Element {
     const [selectedTile, setSelectedTile] = useState<[number, number] | null>(null);
+    const tileRef = useRef<HTMLDivElement | null>(null); // [1, MAP_TILES_X_LEN]のタイル用のuseRef
 
     // タイルがクリックされたときに選択状態にする
     const handleTileClick = (x: number, y: number) => {
         if (selectedTile && selectedTile[0] === x && selectedTile[1] === y) {
             setSelectedTile(null);
-            return;
         } else {
             setSelectedTile([x, y]);
         }
@@ -34,25 +34,27 @@ export function Map(): JSX.Element {
             style={{
                 width: "100%",
                 height: "100%",
-                // clipPath: "inset(5% 5% 5% 5%)",
             }}
         >
             {/* gridを使って実装 */}
             {Array.from({ length: MAP_TILES_Y_LEN + MAP_TILES_X_LEN }, (_, y) =>
                 Array.from({ length: MAP_TILES_Y_LEN + MAP_TILES_X_LEN }, (_, x) => {
                     if (x + y <= MAP_TILES_X_LEN || x + y >= MAP_TILES_X_LEN + 2 * MAP_TILES_Y_LEN) {
-                        // 上を切る、下を切る
                         return null;
                     } else if (x - y <= -MAP_TILES_X_LEN || x - y >= MAP_TILES_X_LEN) {
-                        // 左を切る、右を切る
                         return null;
                     }
+
                     const isSelected = selectedTile?.[0] === x && selectedTile?.[1] === y;
                     const tileTopOffset = isSelected ? SELECTED_TILE_MOVE_Y : 0;
+
+                    // [1, MAP_TILES_X_LEN]のタイルにrefを設定
+                    const isTargetTile = x === 1 && y === MAP_TILES_X_LEN;
 
                     return (
                         <div
                             key={`${x},${y}`}
+                            ref={isTargetTile ? tileRef : null} // 対象のタイルにrefを設定
                             style={{
                                 width: TILE_SIZE,
                                 height: TILE_SIZE,
@@ -76,7 +78,6 @@ export function Map(): JSX.Element {
                             <div>
                                 {x},{y}
                             </div>
-
                             {/* 画像を表示する場合 */}
                             {/* <img
                                 src={image}
